9d51d0bd0d7fd3c6e28298b726f74ad2ed1b41cf
julio Cesar jara ramirez <juliocesar216@gmail.com>
2018-06-12 21:05:00 -0400

    ejercicio corresponde al 2 punto 5

---
+++ ejercicio2_p5.rb
@@ -0,0 +1,13 @@
+#5. Eliminar el producto 'galletas' del hash.
+#array
+
+ # ejercicio corresponde al 2 punto 5
+
+productos = { bebida: 850, chocolate: 1200, leche: 750, cereal: 2200}
+
+productos.each { |valor, producto| puts productos }
+
+
+
+
+
